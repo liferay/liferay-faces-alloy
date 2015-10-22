09c91ea813823a5246c105cf6b4dd89cf80af507
Neil Griffin <neil.griffin.scm@gmail.com>
2015-10-22 13:05:00 -0400

    Added default test in the 1.0.x branch for CI server

---
+++ alloy/src/test/com/liferay/faces/alloy/internal/Test.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.internal;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class AlloyTest {
+
+	@Test
+	public void firstTest() {
+
+		Assert.assertTrue(1 == 1);
+	}
+}
