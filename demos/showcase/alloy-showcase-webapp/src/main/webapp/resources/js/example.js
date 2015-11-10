7fe8ef8cdac167e31a4782ae3bce0ecf733c536e
Neil Griffin <neil.griffin.scm@gmail.com>
2015-11-10 18:14:55 -0500

    FACES-2513 Create the jsf-showcase-webapp project and layer alloy-showcase-webapp on top of it

--- demos/showcase/alloy-showcase-webapp/src/main/webapp/resources/js/example.js
+++
@@ -1,6 +0,0 @@
-var exampleNamespace = {
-	exampleFunction : function(id) {
-		document.getElementById(id).innerHTML = 'A script has called <code>exampleNamespace.exampleFunction(id)</code> '
-				+ 'which resides in the JavaScript resouce file named <code>example.js</code>.';
-	}
-};
