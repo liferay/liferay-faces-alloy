89f6b6ee37222897baf4d1b594bf24a06bdd7cf8
Neil Griffin <neil.griffin.scm@gmail.com>
2015-12-04 18:49:37 -0500

    FACES-2513 Create the jsf-showcase-webapp project and layer alloy-showcase-webapp on top of it (fixed internationalization)

--- alloy/src/main/java/com/liferay/faces/alloy/context/internal/MessageContextAlloyImpl.java
+++ alloy/src/main/java/com/liferay/faces/alloy/context/internal/MessageContextAlloyImpl.java
@@ -21,6 +21,7 @@ import java.util.ResourceBundle;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.liferay.faces.util.context.MessageContext;
+import com.liferay.faces.util.context.MessageContextBundleBase;
 import com.liferay.faces.util.context.MessageContextWrapper;
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
@@ -29,86 +30,14 @@ import com.liferay.faces.util.logging.LoggerFactory;
 /**
  * @author  Neil Griffin
  */
-public class MessageContextAlloyImpl extends MessageContextWrapper {
-
-	// Logger
-	private static final Logger logger = LoggerFactory.getLogger(MessageContextAlloyImpl.class);
-
-	// Private Data Members
-	private boolean initialized;
-	private MessageContext wrappedMessageContext;
-	private Map<String, String> messageMap;
+public class MessageContextAlloyImpl extends MessageContextBundleBase {
 
 	public MessageContextAlloyImpl(MessageContext messageContext) {
-		this.wrappedMessageContext = messageContext;
-		this.messageMap = new ConcurrentHashMap<String, String>();
+		super(messageContext);
 	}
 
 	@Override
-	public String getMessage(Locale locale, String messageId) {
-
-		String message = null;
-		String key = messageId;
-
-		if (locale != null) {
-			key = locale.toString() + messageId;
-		}
-
-		if (messageMap.containsKey(key)) {
-			message = messageMap.get(key);
-
-			if ("".equals(message)) {
-				message = null;
-			}
-		}
-		else {
-
-			ResourceBundle resourceBundle = null;
-
-			try {
-				resourceBundle = ResourceBundle.getBundle("aui-i18n", locale);
-			}
-			catch (MissingResourceException e) {
-
-				if (!initialized) {
-					logger.error(e);
-				}
-			}
-
-			initialized = true;
-
-			if (resourceBundle != null) {
-
-				try {
-					message = resourceBundle.getString(messageId);
-					messageMap.put(key, message);
-				}
-				catch (MissingResourceException e) {
-					messageMap.put(key, "");
-				}
-			}
-		}
-
-		if (message == null) {
-			message = super.getMessage(locale, messageId);
-		}
-
-		return message;
-	}
-
-	@Override
-	public String getMessage(Locale locale, String messageId, Object... arguments) {
-		String message = getMessage(locale, messageId);
-
-		if (message != null) {
-			message = MessageFormat.format(message, arguments);
-		}
-
-		return message;
-	}
-
-	@Override
-	public MessageContext getWrapped() {
-		return wrappedMessageContext;
+	public String getBundleKey() {
+		return "i18n-alloy";
 	}
 }
