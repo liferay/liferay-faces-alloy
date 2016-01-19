fd05fffb20741a06d18871e00c7c100dae8b6caa
Kyle Stiemann <stiemannkj1@gmail.com>
2016-01-19 15:53:59 -0500

    FACES-2544 Change API of the ResourceDependencyVerifier.isResourceDependencySatisfied(UIComponent) to ResourceVerifier.isDependencySatisfied(FacesContext, UIComponent)

---
+++ alloy/src/main/java/com/liferay/faces/alloy/application/internal/ResourceVerifierAlloyImpl.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.application.internal;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import com.liferay.faces.util.application.ResourceUtil;
+import com.liferay.faces.util.application.ResourceVerifier;
+import com.liferay.faces.util.application.ResourceVerifierWrapper;
+import com.liferay.faces.util.product.ProductConstants;
+import com.liferay.faces.util.product.ProductMap;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class ResourceVerifierAlloyImpl extends ResourceVerifierWrapper {
+
+	// Private Constants
+	private static final boolean LIFERAY_PORTAL_DETECTED = ProductMap.getInstance().get(ProductConstants.LIFERAY_PORTAL)
+		.isDetected();
+	private static final Set<String> LIFERAY_PORTAL_INCLUDED_RESOURCE_IDS = new HashSet<String>();
+
+	static {
+		LIFERAY_PORTAL_INCLUDED_RESOURCE_IDS.add(ResourceUtil.getResourceId("liferay-faces-alloy-reslib",
+				"build/aui/aui-min.js"));
+		LIFERAY_PORTAL_INCLUDED_RESOURCE_IDS.add(ResourceUtil.getResourceId("liferay-faces-alloy-reslib",
+				"build/aui-css/css/bootstrap.min.css"));
+		LIFERAY_PORTAL_INCLUDED_RESOURCE_IDS.add(ResourceUtil.getResourceId("liferay-faces-alloy-reslib",
+				"liferay.js"));
+	}
+
+	// Private Members
+	private ResourceVerifier wrappedResourceVerifier;
+
+	public ResourceVerifierAlloyImpl(ResourceVerifier wrappedResourceVerifier) {
+		this.wrappedResourceVerifier = wrappedResourceVerifier;
+	}
+
+	@Override
+	public boolean isDependencySatisfied(FacesContext facesContext, UIComponent componentResource) {
+
+		boolean dependencySatisfied;
+
+		if (LIFERAY_PORTAL_DETECTED &&
+				LIFERAY_PORTAL_INCLUDED_RESOURCE_IDS.contains(ResourceUtil.getResourceId(componentResource))) {
+			dependencySatisfied = true;
+		}
+		else {
+			dependencySatisfied = super.isDependencySatisfied(facesContext, componentResource);
+		}
+
+		return dependencySatisfied;
+	}
+
+	@Override
+	public ResourceVerifier getWrapped() {
+		return wrappedResourceVerifier;
+	}
+}
---
+++ alloy/src/main/java/com/liferay/faces/alloy/application/internal/ResourceVerifierFactoryAlloyImpl.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.alloy.application.internal;
+
+import com.liferay.faces.util.application.ResourceVerifier;
+import com.liferay.faces.util.application.ResourceVerifierFactory;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class ResourceVerifierFactoryAlloyImpl extends ResourceVerifierFactory {
+
+	// Private Members
+	private ResourceVerifierFactory wrappedResourceVerifierFactory;
+
+	public ResourceVerifierFactoryAlloyImpl(ResourceVerifierFactory resourceVerifierFactory) {
+		this.wrappedResourceVerifierFactory = resourceVerifierFactory;
+	}
+
+	@Override
+	public ResourceVerifier getResourceVerifier() {
+
+		ResourceVerifier resourceVerifier = wrappedResourceVerifierFactory.getResourceVerifier();
+
+		return new ResourceVerifierAlloyImpl(resourceVerifier);
+	}
+
+	@Override
+	public ResourceVerifierFactory getWrapped() {
+		return wrappedResourceVerifierFactory;
+	}
+}
