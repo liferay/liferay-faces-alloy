692b5ada307d81f4330599433d3aac9fbcd3fe9d
Kyle Stiemann <stiemannkj1@gmail.com>
2016-07-11 09:09:40 -0400

    FACES-2790 alloy-showcase fails to load due to infinite loop of requests in Pluto (fixed regression which caused inputSourceCode syntax highlighting files to fail to load)

--- reslib/src/main/java/com/liferay/faces/alloy/reslib/application/internal/ResLibResourceHandler.java
+++ reslib/src/main/java/com/liferay/faces/alloy/reslib/application/internal/ResLibResourceHandler.java
@@ -31,6 +31,8 @@ import com.liferay.faces.alloy.reslib.config.ResLibConfigParam;
 import com.liferay.faces.util.config.ApplicationConfig;
 import com.liferay.faces.util.config.ConfiguredServletMapping;
 import com.liferay.faces.util.config.FacesConfig;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
 import com.liferay.faces.util.product.Product;
 import com.liferay.faces.util.product.ProductFactory;
 
@@ -46,10 +48,16 @@ public class ResLibResourceHandler extends ResourceHandlerWrapper {
 	// Public Constants
 	public static final String LIBRARY_NAME = "liferay-faces-alloy-reslib";
 
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(ResLibResourceHandler.class);
+
 	// Private Constants
+	private static final String ACE_EDITOR_SCRIPTS_DIRECTORY = "aui-ace-editor/ace";
 	private static final String LIFERAY_JS = "liferay.js";
 	private static final boolean LIFERAY_PORTAL_DETECTED = ProductFactory.getProduct(Product.Name.LIFERAY_PORTAL)
 		.isDetected();
+	private static final String SCRIPT_RESOURCE_NAME = "script";
+	private static final String ROOT_RESOURCE_DIRECTORY = "build/";
 
 	// Private Members
 	private ResourceHandler wrappedResourceHandler;
@@ -70,20 +78,46 @@ public class ResLibResourceHandler extends ResourceHandlerWrapper {
 				FacesContext facesContext = FacesContext.getCurrentInstance();
 				ExternalContext externalContext = facesContext.getExternalContext();
 				List<String> modulePaths = getModulePaths(externalContext);
-				resource = new ComboResource(modulePaths);
+
+				if (modulePaths.isEmpty()) {
+
+					// Create a dummy resource so that liferay.js can obtain the base combo resource path.
+					resource = new ComboResource();
+				}
+				else {
+					resource = new ComboResource(modulePaths);
+				}
 			}
-			else if (ScriptResource.RESOURCE_NAME.equals(resourceName)) {
+			else if (SCRIPT_RESOURCE_NAME.equals(resourceName)) {
 
 				FacesContext facesContext = FacesContext.getCurrentInstance();
 				ExternalContext externalContext = facesContext.getExternalContext();
 				List<String> modulePaths = getModulePaths(externalContext);
-				String scriptModulePath = "";
 
-				if (modulePaths.size() > 0) {
-					scriptModulePath = modulePaths.get(0);
+				if (modulePaths.isEmpty()) {
+
+					// Create a dummy resource so that liferay.js can obtain the base script resource path.
+					resource = new ReslibResource("script-resource.txt", SCRIPT_RESOURCE_NAME);
 				}
+				else {
 
-				resource = new ScriptResource(scriptModulePath);
+					if (modulePaths.size() > 1) {
+						logger.warn(
+							"The Alloy Reslib script resource cannot be used to obtain multiple script resources in one request. Use the combo resource to obtain multiple script resources combined in one request. Returning the first requested script.");
+					}
+
+					resourceName = modulePaths.get(0);
+
+					// The AceEditor does not prepend the root directory before requesting resources, so add the root
+					// to the beginning of the resource name in order to obtain AceEditor resources. See liferay.js and
+					// https://github.com/liferay/alloy-ui/blob/3.0.3-deprecated.38/src/aui-ace-editor/js/aui-ace-editor.js#L7
+					// for more details.
+					if (resourceName.startsWith(ACE_EDITOR_SCRIPTS_DIRECTORY)) {
+						resourceName = ROOT_RESOURCE_DIRECTORY + resourceName;
+					}
+
+					resource = super.createResource(resourceName, LIBRARY_NAME);
+				}
 			}
 			else {
 
@@ -108,7 +142,7 @@ public class ResLibResourceHandler extends ResourceHandlerWrapper {
 
 		if (!LIFERAY_PORTAL_DETECTED && LIBRARY_NAME.equals(libraryName)) {
 
-			if (ComboResource.RESOURCE_NAME.equals(resourceName) || ScriptResource.RESOURCE_NAME.equals(resourceName)) {
+			if (ComboResource.RESOURCE_NAME.equals(resourceName) || SCRIPT_RESOURCE_NAME.equals(resourceName)) {
 				resource = createResource(resourceName);
 			}
 			else {
@@ -134,7 +168,7 @@ public class ResLibResourceHandler extends ResourceHandlerWrapper {
 
 		if (!LIFERAY_PORTAL_DETECTED && LIBRARY_NAME.equals(libraryName)) {
 
-			if (ComboResource.RESOURCE_NAME.equals(resourceName) || ScriptResource.RESOURCE_NAME.equals(resourceName)) {
+			if (ComboResource.RESOURCE_NAME.equals(resourceName) || SCRIPT_RESOURCE_NAME.equals(resourceName)) {
 				resource = createResource(resourceName);
 			}
 			else {
@@ -174,8 +208,7 @@ public class ResLibResourceHandler extends ResourceHandlerWrapper {
 			// If the resource that is to be rendered is a combo or script module resource and the module paths
 			// extensions are invalid, then the resource cannot be found.
 			if (LIBRARY_NAME.equals(libraryName) &&
-					(ComboResource.RESOURCE_NAME.equals(resourceName) ||
-						ScriptResource.RESOURCE_NAME.equals(resourceName)) &&
+					(ComboResource.RESOURCE_NAME.equals(resourceName) || SCRIPT_RESOURCE_NAME.equals(resourceName)) &&
 					!areModulePathsExtensionsValid(externalContext)) {
 
 				externalContext.setResponseHeader("Cache-Control", "private, no-cache, no-store, must-revalidate");
@@ -245,7 +278,12 @@ public class ResLibResourceHandler extends ResourceHandlerWrapper {
 		while (requestParameterNames.hasNext()) {
 			String parameterName = requestParameterNames.next();
 
-			if (parameterName.startsWith("build/")) {
+			// The AceEditor does not prepend the root directory before requesting resources, so a valid module path
+			// may begin with the root scripts directory or the AceEditor's scripts directory. See liferay.js and
+			// https://github.com/liferay/alloy-ui/blob/3.0.3-deprecated.38/src/aui-ace-editor/js/aui-ace-editor.js#L7
+			// for more details.
+			if (parameterName.startsWith(ROOT_RESOURCE_DIRECTORY) ||
+					parameterName.startsWith(ACE_EDITOR_SCRIPTS_DIRECTORY)) {
 				modulePaths.add(parameterName);
 			}
 		}
