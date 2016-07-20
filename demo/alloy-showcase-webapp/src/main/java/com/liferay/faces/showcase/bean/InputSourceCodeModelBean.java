0b6259aaa656c301165e4ccdb1abae9f36fa5fe0
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-20 14:01:42 -0400

    FACES-2453 ViewScoped beans are not Serializable (reverted from commit 3c05e462e2bcc2075c784e88c29f3b5a4bb29658)

--- demo/alloy-showcase-webapp/src/main/java/com/liferay/faces/showcase/bean/InputSourceCodeModelBean.java
+++ demo/alloy-showcase-webapp/src/main/java/com/liferay/faces/showcase/bean/InputSourceCodeModelBean.java
@@ -13,8 +13,6 @@
  */
 package com.liferay.faces.showcase.bean;
 
-import java.io.Serializable;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
@@ -25,10 +23,7 @@ import javax.faces.context.FacesContext;
  */
 @ManagedBean
 @RequestScoped
-public class InputSourceCodeModelBean implements Serializable {
-
-	// serialVersionUID
-	private static final long serialVersionUID = 2351729189955624081L;
+public class InputSourceCodeModelBean {
 
 	private String sourceCode;
 	private String sourceText;
