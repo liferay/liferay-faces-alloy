d9f65a647dc05c2e9b7f31f9e201aa11f1d5b2c9
Kyle Stiemann <stiemannkj1@gmail.com>
2016-11-30 17:22:54 -0500

    FACES-2968 Allow showcase tests to be run from their respective repos

---
+++ demo/alloy-showcase-webapp/src/test/java/com/liferay/faces/test/alloy/showcase/accordion/AccordionGeneralTester.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.test.alloy.showcase.accordion;
+
+import com.liferay.faces.test.showcase.TesterBase;
+import org.junit.Assume;
+import org.junit.Test;
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class AccordionGeneralTester extends TesterBase {
+
+	@Test
+	public void runAccordionGeneralTest() {
+
+		// TODO
+		Assume.assumeTrue(false);
+	}
+}
